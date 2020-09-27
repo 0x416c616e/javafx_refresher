983495498d49f282fd2077934d0942b0ddf342b8
0x416c616e <29168822+0x416c616e@users.noreply.github.com>
2020-09-27 06:40:18 -0500

    finished the vbs/bat with the relative path to launch the jar

--- java15_jar_test/src/Main.java
+++ java15_jar_test/src/Main.java
@@ -1,5 +1,9 @@
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+
 
 public class Main extends Application {
     @Override
@@ -12,6 +16,11 @@ public class Main extends Application {
         stage.setTitle("test java15 jar");
         stage.setWidth(500);
         stage.setHeight(500);
+        VBox root = new VBox();
+        Label label1 = new Label("I got the JAR to work on java15/openjfx15!");
+        root.getChildren().add(label1);
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
         stage.show();
     }
 
